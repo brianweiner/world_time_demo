b7026db00eab7f34470f8a4e2023a878795f110e
Brian Weiner <brian@320ny.com>
2017-01-26 15:51:33 -0500

    removed countries

--- app/controllers/countries_controller.rb
+++
@@ -1,9 +0,0 @@
-class CountriesController < ApplicationController
-  include DisplayCase::ExhibitsHelper
-  # ...
-  def index
-    # display_case automatically wraps the individual objects contained in
-    # an Enumerable or ActiveRecord::Relation collections
-    @countries = exhibit(Country.all)
-  end
-end
--- app/exhibits/country_exhibit.rb
+++
@@ -1,5 +0,0 @@
-class CountryExhibit < DisplayCase::Exhibit
-  def self.applicable_to?(object, context)
-    object.class.name == 'Country'
-  end
-end
